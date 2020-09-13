d0dbbb3033088200ec952306290b155f5b87ef5a
borislavmm <b2001bobby@gmail.com>
2020-09-13 12:12:04 +0300

    Added some AOP loggings

--- WebCustomerTracker/src/main/java/com/borislavmm/WebCustomerTrackerApplication.java
+++ WebCustomerTracker/src/main/java/com/borislavmm/WebCustomerTrackerApplication.java
@@ -2,9 +2,11 @@ package com.borislavmm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class WebCustomerTrackerApplication {
 
 	public static void main(String[] args) {
