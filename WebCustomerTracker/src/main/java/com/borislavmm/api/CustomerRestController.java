c9a402474a037b5df8c8d75d58313d98d3311b18
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2021-03-03 10:05:18 +0200

    Implemented /customers/{id} (get customer by id).

--- WebCustomerTracker/src/main/java/com/borislavmm/api/CustomerRestController.java
+++ WebCustomerTracker/src/main/java/com/borislavmm/api/CustomerRestController.java
@@ -1,13 +1,16 @@
 package com.borislavmm.api;
 
 import com.borislavmm.entity.Customer;
+import com.borislavmm.exceptions.CustomerNotFoundException;
 import com.borislavmm.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api")
@@ -20,4 +23,18 @@ public class CustomerRestController {
     public List<Customer> getAllCustomers(){
         return customerService.getCustomers();
     }
+
+    @GetMapping("/customers/{id}")
+    public Customer getSingleCustomer(@PathVariable long id){
+        Customer customer = null;
+
+        try {
+            customer = customerService.getCustomerById(id);
+        }catch (NoSuchElementException e){
+            throw new CustomerNotFoundException("Customer id is not found: " + id);
+        }
+
+        return customer;
+    }
+
 }
