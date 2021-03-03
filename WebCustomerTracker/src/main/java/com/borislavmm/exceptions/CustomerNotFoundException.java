ba94f04950080187a12027c799c121eb6a502509
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2021-03-03 10:02:46 +0200

    Added exception classes

---
+++ WebCustomerTracker/src/main/java/com/borislavmm/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,17 @@
+package com.borislavmm.exceptions;
+
+public class CustomerNotFoundException extends RuntimeException{
+
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+
+    public CustomerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomerNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
