720f95fc272826a1630472ca564e71c0b1ef0c32
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2022-06-10 11:55:08 +0300

    Some minor changes

--- WebCustomerTracker/src/main/java/com/borislavmm/bootstrap/BootstrapApp.java
+++ WebCustomerTracker/src/main/java/com/borislavmm/bootstrap/BootstrapApp.java
@@ -4,6 +4,7 @@ import com.borislavmm.entity.Customer;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.URI;
@@ -13,9 +14,14 @@ import java.util.*;
 public class BootstrapApp implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
-//        Customer c = WebClient.create("http://localhost:8080/api/customers").get().uri("/13")
-//                .retrieve().bodyToFlux(Customer.class).blockFirst();
-//        System.out.println(c.getId() +" " +c.getFirstName());
+        Customer c = WebClient.create("http://localhost:8080/api/customers").get().uri("/13")
+                .retrieve().bodyToFlux(Customer.class).blockFirst();
+
+//        Customer c = new RestTemplate().getForObject("http://localhost:8080/api/customers/13",  Customer.class);
+        System.out.println(c.getId() +" " +c.getFirstName());
+
+
+
 //        for (Customer c : customers) {
 //            System.out.println(c.getId() +" " +c.getFirstName());
 //        }
